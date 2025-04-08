d0f1fa9bf5f4bfeda0fcc38cc725fe5fd1dde2b1
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-04-08 15:53:23 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 142 ms, Memory: 33,920 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -4,36 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int t = 1; t<=T ; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int[] arr = new int[N];
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			for(int i=0;i<N;i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			int max = 0;
-			int[] dp = new int[N];
-			
-			for(int i=0;i<N;i++) {
-				dp[i] = 1;
-				for(int j=0;j<i;j++) {
-					if(arr[j]<arr[i]) {
-						dp[i] = Math.max(dp[i], dp[j]+1);
-					}
-				}
-				max = Math.max(dp[i],max);
-				
-			}
-			
-			
-			System.out.println("#"+t+" "+max);
-		}
-		
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int[] dp = new int[N];
+            int max = 0;
+
+            // LIS 알고리즘 부분
+            for (int i = 0; i < N; i++) {
+                dp[i] = 1; // 자기 자신 하나로 시작하는 수열
+                for (int j = 0; j < i; j++) {
+                    if (arr[j] < arr[i]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1); // 이전 중 작은 수 +1
+                    }
+                }
+                max = Math.max(max, dp[i]); // 지금까지의 LIS 최대 길이 저장
+            }
+
+            System.out.println("#" + t + " " + max);
+        }
+    }
 }

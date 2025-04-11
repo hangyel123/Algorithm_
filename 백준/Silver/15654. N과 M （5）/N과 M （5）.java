a60ce1694dc563fbaa421ad155f3989e2f9bd92c
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-04-11 15:10:05 +0900

    [Silver III] Title: N과 M (5), Time: 296 ms, Memory: 61912 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] result, arr;
+	static boolean[] visited;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+		visited = new boolean[N];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		back(0);
+		bw.flush();
+		bw.close();
+
+	}
+
+	static void back(int depth) throws IOException {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				bw.write(result[i] + " ");
+			}
+			bw.newLine();
+			return;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (!visited[i]) {
+				visited[i]= true;
+				result[depth] = arr[i];
+				back(depth + 1);
+				visited[i]=false;
+			}
+
+		}
+
+	}
+}

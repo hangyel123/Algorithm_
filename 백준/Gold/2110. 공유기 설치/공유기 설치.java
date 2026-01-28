4455efa21990301ea7ee2152a85e70bef8624829
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2026-01-28 16:59:47 +0900

    [Gold IV] Title: 공유기 설치, Time: 216 ms, Memory: 29920 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -7,46 +7,43 @@ public class Main {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		int N = Integer.parseInt(st.nextToken());
 		int C = Integer.parseInt(st.nextToken());
+		long[] heights= new long[N];
 		
-		long[] positions = new long[N];
-		for(int i=0;i<N;i++) {
-			positions[i] = Long.parseLong(br.readLine().trim());		
+		for(int i =0; i<N;i++) {
+			heights[i] = Long.parseLong(br.readLine().trim());
 		}
-		Arrays.sort(positions);
+		Arrays.sort(heights);
 		
-		long lo = 1;
-		long hi = positions[N-1] - positions[0];
+		long lo = 0;
+		long hi = heights[N-1] - heights[0];
 		long ans = 0;
-		
+				
 		while(lo<=hi) {
-			long mid = (lo+hi)/2;
-			if(canInstall(positions, C, mid)) {
+			long mid = (hi + lo)/2;
+			if(isCanLocate(heights, C, mid)) {
 				ans = mid;
-				lo = mid+1;
+				lo = mid +1;
 			}else {
-				hi = mid -1;
+				hi = mid - 1;
 			}
 		}
 		System.out.println(ans);
 	}
-	static boolean canInstall(long[] pos, int C,long dist) {
-		int count = 1;
+	
+	public static boolean isCanLocate(long[] pos, int C, long dist) {
+		int cnt = 1;
 		long last = pos[0];
 		
-		for(int i = 1;i<pos.length;i++) {
+		for(int i=1;i<pos.length;i++) {
 			if(pos[i]-last >= dist) {
-				count++;
+				cnt++;
 				last = pos[i];
-				
-				if(count >= C) {
-					return true;
-				}
+			}if(cnt >= C) {
+				return true;
 			}
 		}
 		return false;
 	}
-	
 }

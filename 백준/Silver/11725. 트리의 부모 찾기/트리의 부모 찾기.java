d82281f4dfa2a4ca72614f65f249d215ac50b51d
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2026-01-29 18:20:59 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 480 ms, Memory: 64756 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
@@ -7,50 +6,43 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-  
-    
-   
     public static void main(String[] args) throws Exception {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	int N = Integer.parseInt(br.readLine().trim());
     	
     	ArrayList<Integer>[] graph = new ArrayList[N+1];
-    	for(int i = 1;i<=N;i++) {
-    		graph[i] = new ArrayList<>();
-    	}
+    	for(int i=1;i<=N;i++) graph[i] = new ArrayList<>();
     	
     	for(int i = 0;i<N-1;i++) {
     		StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            
-            graph[a].add(b);
-            graph[b].add(a);
+        	int a = Integer.parseInt(st.nextToken());
+        	int b = Integer.parseInt(st.nextToken());
+        	
+        	graph[a].add(b);
+        	graph[b].add(a);
     	}
+    	int root = 1;
+    	int[] parents = new int[N+1];
+    	boolean[] isVisited = new boolean[N+1];
+    
+    	ArrayDeque<Integer> queue= new ArrayDeque<>();
+    	queue.offer(root);
+    	isVisited[root] = true;
     	
-    	int[] parent = new int[N+1]; 
-    	boolean[] visited = new boolean[N + 1];
-    	
-    	ArrayDeque<Integer> q = new ArrayDeque<>();
-    	q.offer(1);
-    	visited[1] = true;
-    	
-    	while(!q.isEmpty()) {
-    		int cur = q.poll();
+    	while(!queue.isEmpty()) {
+    		int cur = queue.poll();
     		for(int nxt : graph[cur]) {
-    			if(!visited[nxt]) {
-    				visited[nxt] = true;
-    				parent[nxt] = cur;
-    				q.offer(nxt);
+    			if(!isVisited[nxt]) {
+    				isVisited[nxt] = true;
+    				parents[nxt] = cur;
+    				queue.offer(nxt);
     			}
     		}
     	}
-    	
     	StringBuilder sb = new StringBuilder();
-    	for(int i = 2;i<=N;i++) {
-    		sb.append(parent[i]).append('\n');
+    	for(int i = 2; i<=N;i++) {
+    		sb.append(parents[i]).append('\n');
     	}
-    	
     	System.out.println(sb);
     }
 }
